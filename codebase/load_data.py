6c6ad56af10bc6b4688049182a5bd25e965b55bd
Ann Scheliga <ann_scheliga@berkeley.edu>
2023-09-28 09:27:08 -0700

    tidying package imports

--- codebase/load_data.py
+++ codebase/load_data.py
@@ -1,7 +1,5 @@
 def load_CYGNSS_05(cygnss_filename = 'CYGNSS_watermask_0_5_with_lakes.nc',
-    cygnss_filepath = '/global/scratch/users/cgerlein/fc_ecohydrology_scratch/CYGNSS/Data/CYGNSS_L1_v3_1_data_products/Monthly_maps_watermasks_glob_netCDF/WetCHARTs_size_0_5_deg/',
-    # xarray_loc = '/global/home/users/ann_scheliga/.local/lib/python3.7/site-packages/xarray'
-    ):
+    cygnss_filepath = '/global/scratch/users/cgerlein/fc_ecohydrology_scratch/CYGNSS/Data/CYGNSS_L1_v3_1_data_products/Monthly_maps_watermasks_glob_netCDF/WetCHARTs_size_0_5_deg/'):
     """
     Inputs
     ------
@@ -15,7 +13,6 @@ def load_CYGNSS_05(cygnss_filename = 'CYGNSS_watermask_0_5_with_lakes.nc',
     fw : xarray.DataArray
     """
     import sys
-    # sys.path.append(xarray_loc)
     import xarray as xr
     import matplotlib.pyplot as plt
     cygnss_raw = xr.open_dataset(cygnss_filepath+cygnss_filename,decode_coords='all', decode_times=False)
