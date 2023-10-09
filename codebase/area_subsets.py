7deba8321e3179756b665c5c383f97a9f9df88a7
Ann Scheliga <ann_scheliga@berkeley.edu>
2023-10-09 12:03:44 -0700

    added xr_shape_subset and documentation

--- codebase/area_subsets.py
+++ codebase/area_subsets.py
@@ -14,6 +14,9 @@ def reservoir_name_to_point(dam_name,res_shp,idx = 0):
     ------
     dam_name : str
         name of dam in dataset
+    res_shp : GeoPandas GeoDataFrame
+        from `load_data.load_GRanD()` function
+        dataset of GRanD reservoirs
     idx : int
         default = 0
         use to select a specific dam by index if the dam name appears more than once
@@ -45,7 +48,9 @@ def grace_point_subset(coords_i,grace_dict,buffer_val=0):
     Inputs
     ------
     coords_i: tuple of (lat,lon)
-
+    grace_dict : dictionary of GRACE TWS info
+        from `load_data.load_GRACE()`
+        uses grace_dict 'mascon' and 'cmwe' keys
     buffer_val : float
         default = 0
         units of decimal degrees
@@ -122,9 +127,73 @@ def cygnss_point_subset(coords_i,fw):
     fw_ts = pd.Series(data=fw_xr,index=dates_fw)
     return fw_ts
 def grace_shape_subset(dam_name,res_shp,grace_dict,buffer_val=0):
+    """
+    Inputs
+    ------
+    dam_name : str
+        name of dam in dataset
+    res_shp : GeoPandas GeoDataFrame
+        from `load_data.load_GRanD()` function
+        dataset of GRanD reservoirs
+        used to subset grace_dict mascons
+    grace_dict : dictionary of GRACE TWS info
+        from `load_data.load_GRACE()`
+        uses grace_dict 'mascon' and 'cmwe' keys
+    buffer_val : float
+        default = 0
+        units of decimal degrees
+        the extra length to extend the subset in a square from the central coordinate
+
+    Outputs
+    -------
+    subsetted_cmwe : pd.DataFrame
+        GRACE cmwe solution of the mascons intersecting the input reservoir
+    subsetted_mascon : pd.DataFrame
+        GRACE mascon metadata of the mascons intersecting the input reservoir
+    """
     shape_row = check_for_multiple_dams(dam_name,res_shp)
     shape_poly = shape_row['geometry'].buffer(buffer_val).values[0]
     bool_series = grace_dict['mascon'].intersects(shape_poly)
     subsetted_mascon = grace_dict['mascon'][bool_series]
     subsetted_cmwe = grace_dict['cmwe'][bool_series]
     return subsetted_cmwe , subsetted_mascon
+def xr_shape_subset(dam_name,res_shp,input_xr,buffer_val=0,crs_code = 4326):
+    """
+    Inputs
+    ------
+    dam_name : str
+        name of dam in dataset
+    res_shp : GeoPandas GeoDataFrame
+        from `load_data.load_GRanD()` function
+        dataset of GRanD reservoirs
+        used to subset input_xr
+    input_xr : xarray DataArray
+        must have 'lat' and 'lon' substrings in coord names
+    buffer_val : float
+        default = 0
+        units of decimal degrees
+        the extra length to extend the subset in a square from the central coordinate
+    crs_code : int
+        default = 4326 (WGS84)
+        EPSG code for coordinate reference system
+        crs_code applied to input_xr
+
+    Outputs
+    -------
+    clip_rxr : xarray DataArray
+        input_xr subset to the input reservoir
+    """
+    subset_gpd = check_for_multiple_dams(dam_name,res_shp)
+    # Add crs to xr
+    full_rxr = input_xr.rio.write_crs(crs_code)
+
+    # Grab coordinate names
+    x_name = [dim for dim in list(input_xr.dims) if 'lon' in dim][0]
+    y_name = [dim for dim in list(input_xr.dims) if 'lat' in dim][0]
+
+    # Set spatial dimensions to xr
+    full_rxr.rio.set_spatial_dims(x_name,y_name,inplace=True)
+
+    # Apply shp subset
+    clip_rxr = full_rxr.rio.clip(subset_gpd.geometry.buffer(buffer_val) , subset_gpd.crs)
+    return clip_rxr
