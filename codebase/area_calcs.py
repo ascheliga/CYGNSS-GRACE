a3118ce581d9a16d273d59fe55d643e8454fb67d
Ann Scheliga <ann_scheliga@berkeley.edu>
2024-01-10 12:45:25 -0800

    Wrote load_DEM_subset tests + linting

--- codebase/area_calcs.py
+++ codebase/area_calcs.py
@@ -5,6 +5,7 @@
 import numpy as np
 import pandas as pd
 import xarray as xr
+
 # In[3]:
 
 
--- tests/test_load_DEM_subset.py
+++ tests/test_load_DEM_subset.py
@@ -1,20 +1,33 @@
-# from codebase.load_data import load_DEM_full
+import numpy as np
+import pandas as pd
 
-# def test_DEM_shape():
-#     expected_bbox = (9001,36001)
-#     output_dem , output_lat ,output_lon = load_DEM_subset(bbox)
-#     output_shape = output_dem.shape
-#     np.testing.assert_equal(output_shape, expected_shape)
-#     np.testing_assert_equal(len(output_lat),expected_shape[0])
-#     np.testing_assert_equal(len(output_lon),expected_shape[1])
+from codebase.load_data import load_DEM_subset
 
-# def test_DEM_values():
-#     expected_max = 32767
-#     expected_min = -414.9602
-#     expected_mean = 24395.6754
-#     expected_stats = [expected_max , expected_min, expected_mean]
-#     output_dem , _ , _ = load_DEM_full()
 
-#     output_stats = [output_dem.max(), output_dem.min(), output_dem.mean()]
+def test_DEM_shape():
+    test_bbox = pd.DataFrame(
+        data=np.array([10, 15, 30, 25]).reshape(1, -1),
+        columns=["minx", "miny", "maxx", "maxy"],
+    )
+    expected_shape = (1001, 2001)
+    output_dem, output_lat, output_lon = load_DEM_subset(test_bbox)
+    output_shape = output_dem.shape
+    np.testing.assert_equal(output_shape, expected_shape)
+    np.testing.assert_equal(len(output_lat), expected_shape[0])
+    np.testing.assert_equal(len(output_lon), expected_shape[1])
 
-#     np.testing_assert_allclose(output_stats,expected_stats,rtol = 1e-2)
+
+def test_DEM_values():
+    test_bbox = pd.DataFrame(
+        data=np.array([10, 15, 30, 25]).reshape(1, -1),
+        columns=["minx", "miny", "maxx", "maxy"],
+    )
+    expected_max = 3304.5072
+    expected_min = 151.6720
+    expected_mean = 566.2700
+    expected_stats = [expected_max, expected_min, expected_mean]
+    output_dem, _, _ = load_DEM_subset(test_bbox)
+
+    output_stats = [output_dem.max(), output_dem.min(), output_dem.mean()]
+
+    np.testing.assert_allclose(output_stats, expected_stats, rtol=1e-2)
