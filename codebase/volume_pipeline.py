ec9b3e1fc521816724c7f37b05e79ac9ea5f1051
Ann Scheliga <ann_scheliga@berkeley.edu>
2024-02-26 11:38:48 -0800

    linting of subset DEM & CYGNSS functions

--- codebase/volume_pipeline.py
+++ codebase/volume_pipeline.py
@@ -1,36 +1,53 @@
+from geopandas import GeoDataFrame
 from xarray import DataArray
+
+
 # 1a. subset fw data
 # 1b. subset DEM data
 # 1c. Align DEM and fw data subsets
-def subset_DEM_and_CYGNSS_data_from_name(dam_name: str) -> tuple[DataArray]:
-    from . import load_data
-    from . import area_subsets
-    subset_gpd = area_subsets.check_for_multiple_dams(dam_name,res_shp)
+def subset_DEM_and_CYGNSS_data_from_name(
+    dam_name: str, res_shp: GeoDataFrame
+) -> tuple[DataArray, DataArray]:
+    from . import area_subsets, load_data
+
+    subset_gpd = area_subsets.check_for_multiple_dams(dam_name, res_shp)
     subset_bbox = subset_gpd.geometry.buffer(0).bounds
     dem_DA = load_data.load_DEM_subset_as_rxrDA(subset_bbox)
     fw_DA = load_data.load_CYGNSS_001_all_months(subset_bbox)
-    return dem_DA , fw_DA
+    return dem_DA, fw_DA
 
-def align_DEM_and_CYGNSS_coordinates(dem_DA: DataArray, fw_DA: DataArray) -> tuple[DataArray]:
+
+def align_DEM_and_CYGNSS_coordinates(
+    dem_DA: DataArray, fw_DA: DataArray
+) -> tuple[DataArray, DataArray]:
     from numpy import testing
-    testing.assert_allclose(dem_DA['lat'].values, fw_DA['lat'].values)
-    dem_DA['lat'] = fw_DA['lat']
-    testing.assert_allclose(dem_DA['lon'].values, fw_DA['lon'].values)
-    dem_DA['lon'] = fw_DA['lon']
-    return dem_DA , fw_DA
+
+    testing.assert_allclose(dem_DA["lat"].values, fw_DA["lat"].values)
+    dem_DA["lat"] = fw_DA["lat"]
+    testing.assert_allclose(dem_DA["lon"].values, fw_DA["lon"].values)
+    dem_DA["lon"] = fw_DA["lon"]
+    return dem_DA, fw_DA
+
 
 def format_CYGNSS_data_to_binary(fw_DA: DataArray) -> DataArray:
-    from . import area_calcs
-    from . import time_series_calcs
+    from . import area_calcs, time_series_calcs
+
     fw_DA_binary = area_calcs.cygnss_convert_to_binary(fw_DA)
-    fw_DA_binary['time'] = time_series_calcs.CYGNSS_timestep_to_pdTimestamp(fw_DA['time'])
+    fw_DA_binary["time"] = time_series_calcs.CYGNSS_timestep_to_pdTimestamp(
+        fw_DA["time"]
+    )
     return fw_DA_binary
 
-def subset_and_align_DEM_CYGNSS_data(dam_name: str) -> tuple[DataArray]:
-    dem_DA , fw_DA = subset_DEM_and_CYGNSS_data_from_name(dam_name)
-    dem_DA , fw_DA = align_DEM_and_CYGNSS_coordinates(dem_DA, fw_DA)
+
+def create_aligned_DEM_CYGNSS_subsets(
+    dam_name: str, res_shp: GeoDataFrame
+) -> tuple[DataArray, DataArray]:
+    dem_DA, fw_DA = subset_DEM_and_CYGNSS_data_from_name(dam_name, res_shp)
+    dem_DA, fw_DA = align_DEM_and_CYGNSS_coordinates(dem_DA, fw_DA)
     fw_DA = format_CYGNSS_data_to_binary(fw_DA)
-    return dem_DA , fw_DA
+    return dem_DA, fw_DA
+
+
 # 2. Create change over time fw dataArray
 
 # 3. Assign each time step as shrinking or expanding
