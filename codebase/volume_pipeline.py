0344beee5f2acc24acfc42982ebd4c0395460a4a
Ann Scheliga <ann_scheliga@berkeley.edu>
2024-02-26 11:14:52 -0800

    Outline for volume pipeline code

---
+++ codebase/volume_pipeline.py
@@ -0,0 +1,16 @@
+# 1a. subset fw data
+# 1b. subset DEM data
+# 1c. Align DEM and fw data subsets
+
+# 2. Create change over time fw dataArray
+
+# 3. Assign each time step as shrinking or expanding
+
+# 4a. Fit distribution of start timestep DEM
+# 4b. Fit distribution of shrink/expand DEM
+
+# 5. Calculate change in height from difference in distribution
+
+# 6. Calculate area of start timestep DEM
+
+# 7. Calculate volume from change in height x area
