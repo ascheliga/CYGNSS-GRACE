1d08fe206b58199eb28df8ed344905ad69c0ac0e
Ann Scheliga <ann_scheliga@berkeley.edu>
2024-05-20 11:58:19 -0700

    moving functions in different modules

--- codebase/load_data.py
+++ codebase/load_data.py
@@ -512,21 +512,11 @@ def load_usbr_data(
     return raw_data
 
 
-def usbr_dataprocessing(df: pd.DataFrame) -> pd.DataFrame:
-    from utils import convert_to_num
-
-    df = df.drop(columns="Location")
-    df.dropna(axis=0, how="any", inplace=True)
-    df = df[df["Timestep"] != "Timestep"]  # remove repeat header rows
-    df["Variable"] = df["Parameter"] + " [" + df["Units"] + "]"
-    df["Result"] = df["Result"].apply(convert_to_num)
-    df_pivot = df.pivot(columns="Variable", index="Datetime (UTC)", values="Result")
-    return df_pivot
-
-
 def load_formatted_usbr_data(
     name: str, file_dir: str = "/global/scratch/users/ann_scheliga/dam_datasets/"
 ) -> pd.DataFrame:
+    from dataprocessing import usbr_dataprocessing
+
     raw_data = load_usbr_data(name, file_dir)
     data = usbr_dataprocessing(raw_data)
     return data
