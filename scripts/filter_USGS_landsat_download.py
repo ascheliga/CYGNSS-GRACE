c0fc401ee839850bc3612aa6ad8be97fc457ea81
Ann Scheliga <ann_scheliga@berkeley.edu>
2024-10-08 12:38:14 -0700

    Moved landsat processing to codebase and scripts

---
+++ scripts/filter_USGS_landsat_download.py
@@ -0,0 +1,24 @@
+# Purpose: Read the original file and create new shortened file
+
+# Location of USGS shell script
+sh_fp = '/global/scratch/users/ann_scheliga/aux_dam_datasets/Landsat8/'
+sh_fn = '0373424596-download.sh'
+
+# Grab original content and make edits
+with open(sh_fp+sh_fn,"r") as f:
+    # Create a list of file contents (instead of a string)
+    full_file = f.readlines()
+    # Split content into sections
+    front_matter = full_file[:97]
+    urls = full_file[97:-1]
+    back_matter = full_file[-1:]
+    # Set filters for urls
+    filter_strs = ['B03','B05','Fmask']
+    # Select only urls that contain any filter value
+    urls_shortened = [url for url in urls if any(band in url for band in filter_strs)]
+    print(len(urls_shortened))
+    final_file = front_matter + urls_shortened + back_matter
+
+# # Write the editted script to a new file.
+with open(sh_fp + 'download_script_B03_B05.sh',"w") as f:
+    f.writelines(final_file)
