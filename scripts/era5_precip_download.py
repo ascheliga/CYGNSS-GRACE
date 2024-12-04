968755c657021199bf86c3248fd86491cfc27706
Ann Scheliga <ann_scheliga@berkeley.edu>
2024-12-03 20:13:25 -0800

    super commit to deal with large files

--- scripts/era5_precip_download.py
+++ scripts/era5_precip_download.py
@@ -1,18 +1,43 @@
+print('inside script',flush=True)
+
 import cdsapi
+from numpy import arange
 
-dataset = "insitu-gridded-observations-global-and-regional"
-request = {
-    "origin": "gpcc",
-    "region": "global",
-    "variable": ["precipitation"],
-    "time_aggregation": "daily",
-    "horizontal_aggregation": ["1_x_1"],
-    "year": [
-        "2018", "2019", "2020",
-        "2021"
-    ],
-    "version": ["v2020_0_v6_0_fg"]
-}
-
-client = cdsapi.Client()
-client.retrieve(dataset, request).download()
+print('Starting PRECIP')
+year_list = list(arange(2019,2020).astype(str))
+for year_str in year_list:
+    output_file = '/global/scratch/users/ann_scheliga/era5_test_data/' + year_str + 'daiy_tot_precip.nc'
+    print('Starting', year_str,flush=True)
+    dataset = "derived-era5-single-levels-daily-statistics"
+    request = {
+        "product_type": "reanalysis",
+        "variable": ["total_precipitation"],
+        "year": year_str,
+        "month": [
+            "01", "02", "03",
+            "04", "05", "06",
+            "07", "08", "09",
+            "10", "11", "12"
+        ],
+        "day": [
+            "01", "02", "03",
+            "04", "05", "06",
+            "07", "08", "09",
+            "10", "11", "12",
+            "13", "14", "15",
+            "16", "17", "18",
+            "19", "20", "21",
+            "22", "23", "24",
+            "25", "26", "27",
+            "28", "29", "30",
+            "31"
+        ],
+        "daily_statistic": "daily_sum",
+        "time_zone": "utc+00:00",
+        "frequency": "6_hourly",
+        "area": [40, -180, -40, 180]
+    }
+    
+    client = cdsapi.Client()
+    client.retrieve(dataset, request).download(target = output_file)
+    print('Saved to', output_file,flush=True)
--- scripts/era5_temp_download.py
+++ scripts/era5_temp_download.py
@@ -1,34 +1,41 @@
 import cdsapi
+from numpy import arange
 
-dataset = "derived-era5-single-levels-daily-statistics"
-request = {
-    "product_type": "reanalysis",
-    "variable": ["2m_temperature"],
-    "year": "2019",
-    "month": [
-        "01", "02", "03",
-        "04", "05", "06",
-        "07", "08", "09",
-        "10", "11", "12"
-    ],
-    "day": [
-        "01", "02", "03",
-        "04", "05", "06",
-        "07", "08", "09",
-        "10", "11", "12",
-        "13", "14", "15",
-        "16", "17", "18",
-        "19", "20", "21",
-        "22", "23", "24",
-        "25", "26", "27",
-        "28", "29", "30",
-        "31"
-    ],
-    "daily_statistic": "daily_mean",
-    "time_zone": "utc+00:00",
-    "frequency": "6_hourly",
-    "area": [40, -180, -40, 180]
-}
-
-client = cdsapi.Client()
-client.retrieve(dataset, request).download()
+print('Starting TEMPERATURE',flush = True)
+year_list = list(arange(2019,2020).astype(str))
+for year_str in year_list:
+    output_file = '/global/scratch/users/ann_scheliga/era5_test_data/' + year_str + 'daiy_tempK.nc'
+    print('Starting', year_str,flush=True)
+    dataset = "derived-era5-single-levels-daily-statistics"
+    request = {
+        "product_type": "reanalysis",
+        "variable": ["2m_temperature"],
+        "year": year_str,
+        "month": [
+            "01", "02", "03",
+            "04", "05", "06",
+            "07", "08", "09",
+            "10", "11", "12"
+        ],
+        "day": [
+            "01", "02", "03",
+            "04", "05", "06",
+            "07", "08", "09",
+            "10", "11", "12",
+            "13", "14", "15",
+            "16", "17", "18",
+            "19", "20", "21",
+            "22", "23", "24",
+            "25", "26", "27",
+            "28", "29", "30",
+            "31"
+        ],
+        "daily_statistic": "daily_mean",
+        "time_zone": "utc+00:00",
+        "frequency": "6_hourly",
+        "area": [40, -180, -40, 180]
+    }
+    
+    client = cdsapi.Client()
+    client.retrieve(dataset, request).download(target = output_file)
+    print('Saved to', output_file,flush=True)
