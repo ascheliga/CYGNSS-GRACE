7294a466fbcc1aaa150ca406f1c8ac55f4ee013d
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-07-23 21:02:20 -0700

    failing to downlaod era5 data

--- scripts/download_all_era5_met.sh
+++ scripts/download_all_era5_met.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=download_era5_met_systematic_attempt04
+#SBATCH --job-name=download_era5_met_systematic_attempt06
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2_htc
-#SBATCH --time=06:02:35
+#SBATCH --time=05:02:35
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 
@@ -17,8 +17,8 @@ conda activate rio_keras
 #conda activate /global/home/users/ann_scheliga/.conda/envs/rio_keras
 # python era5_monthly_data_downoad.py
 
-export start_year=2010
+export start_year=2015
 export end_year_ex=2024
 # ipython era5_preciptype_download.py
-ipython era5_precip_download.py
+# ipython era5_precip_download.py
 ipython era5_temp_download.py
