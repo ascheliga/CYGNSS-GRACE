a81279219f42c2b6a00980fbd97d33ae22dd4e8c
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-10 17:46:26 -0700

    parallelized with mppool and downloading 2000s

--- scripts/download_all_era5_met.sh
+++ scripts/download_all_era5_met.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=download_era5_met_systematic_attempt08
+#SBATCH --job-name=download_era5_met_2000s_mppool_att00
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2_htc
-#SBATCH --time=05:02:35
+#SBATCH --time=01:00:35
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=3
 
 ## Command(s) to run:
-
-echo "hello world"
-module load python
 
 eval "$(conda shell.bash hook)"
 
@@ -17,8 +14,8 @@ conda activate rio_keras
 #conda activate /global/home/users/ann_scheliga/.conda/envs/rio_keras
 # python era5_monthly_data_downoad.py
 
-export start_year=2015
-export end_year_ex=2024
-# ipython era5_preciptype_download.py
-# ipython era5_precip_download.py
-ipython era5_temp_download.py
+export start_year=2000
+export end_year_ex=2010
+python era5_preciptype_download.py
+python era5_precip_download.py
+python era5_temp_download.py
--- scripts/era5_preciptype_download.py
+++ scripts/era5_preciptype_download.py
@@ -1,45 +1,85 @@
-import cdsapi
 import os
+from multiprocessing import Pool
+
+import cdsapi
 from numpy import arange
 
-start_year = int(os.environ['start_year'])
-end_year_ex = int(os.environ['end_year_ex']) # exclusive of this year
+start_year = int(os.environ["start_year"])
+end_year_ex = int(os.environ["end_year_ex"])  # exclusive of this year
 
-print('Starting PRECIP_TYPE',flush = True)
-year_list = list(arange(start_year,end_year_ex).astype(str))
-for year_str in year_list:
-    output_file = '/global/scratch/users/ann_scheliga/era5_data/' + year_str + 'daily_precip_type.nc'
-    print('Starting', year_str,flush=True)
+print("Starting PRECIP_TYPE", flush=True)
+year_list = list(arange(start_year, end_year_ex).astype(str))
+
+
+def yearly_preciptype_request(year_str: str) -> None:
+    output_file = (
+        "/global/scratch/users/ann_scheliga/era5_data/"
+        + year_str
+        + "daily_precip_type.nc"
+    )
+    print("Starting", year_str, flush=True)
     dataset = "derived-era5-single-levels-daily-statistics"
     request = {
         "product_type": "reanalysis",
         "variable": ["precipitation_type"],
         "year": year_str,
         "month": [
-            "01", "02", "03",
-            "04", "05", "06",
-            "07", "08", "09",
-            "10", "11", "12"
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
         ],
         "day": [
-            "01", "02", "03",
-            "04", "05", "06",
-            "07", "08", "09",
-            "10", "11", "12",
-            "13", "14", "15",
-            "16", "17", "18",
-            "19", "20", "21",
-            "22", "23", "24",
-            "25", "26", "27",
-            "28", "29", "30",
-            "31"
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
         ],
         "daily_statistic": "daily_mean",
         "time_zone": "utc+00:00",
         "frequency": "6_hourly",
-        "area": [40, -180, -40, 180]
+        "area": [40, -180, -40, 180],
     }
-    
+
     client = cdsapi.Client()
-    client.retrieve(dataset, request).download(target = output_file)
-    print('Saved to', output_file,flush=True)
+    client.retrieve(dataset, request).download(target=output_file)
+    print("Saved to", output_file, flush=True)
+
+
+with Pool() as pool:
+    result = pool.map(yearly_preciptype_request, year_list)
