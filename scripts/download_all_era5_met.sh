d71e94e6e184b817a4a629621beb0327a805544e
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-14 12:15:14 -0700

    retrying ERA5 download after timeout

--- scripts/download_all_era5_met.sh
+++ scripts/download_all_era5_met.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=download_era5_dewpoint_att00
+#SBATCH --job-name=download_era5_mintemp_att01
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2_htc
-#SBATCH --time=06:00:35
+#SBATCH --time=05:00:35
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=3
 
@@ -14,10 +14,10 @@ conda activate rio_keras
 #conda activate /global/home/users/ann_scheliga/.conda/envs/rio_keras
 # python era5_monthly_data_downoad.py
 
-export start_year=2000
+export start_year=2016
 export end_year_ex=2024
 # python era5_preciptype_download.py
 # python era5_precip_download.py
-# python era5_max_temp_download.py
+python era5_max_temp_download.py
 # python era5_min_temp_download.py
-python era5_dewpoint_download.py
+# python era5_dewpoint_download.py
